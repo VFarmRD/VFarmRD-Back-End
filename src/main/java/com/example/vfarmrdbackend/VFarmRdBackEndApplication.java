c89180d5ab0198d931709b612b2e2832959fa500
unknown <winnhat2000@gmail.com>
2022-05-16 23:58:53 +0700

    first commit

---
+++ src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.vfarmrdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VFarmRdBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VFarmRdBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vfarmrdbackend/VFarmRdBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vfarmrdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VFarmRdBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
