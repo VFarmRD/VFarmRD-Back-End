480e065c88cf396516eff3334220459b4356e543
minhnln0712 <qazzaq123>
2022-05-26 08:12:48 +0700

    delete cors in auth page and add global cors

--- src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
+++ src/main/java/com/example/vfarmrdbackend/VFarmRdBackEndApplication.java
@@ -2,12 +2,31 @@ package com.example.vfarmrdbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VFarmRdBackEndApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VFarmRdBackEndApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+
+        // return null;
     }
 
 }
