7ea664999e457f3b662bbb62d7d35857ad7558c6
minhnln0712 <qazzaq123>
2022-06-02 10:20:27 +0700

    update set modified time for user when delete or recover

--- src/main/java/com/example/vfarmrdbackend/controllers/UserController.java
+++ src/main/java/com/example/vfarmrdbackend/controllers/UserController.java
@@ -151,6 +151,8 @@ public class UserController {
     public ResponseEntity<?> deleteUser(@PathVariable("id") int id) {
         User _user = userRepository.getUserByUser_id(id);
         if (_user != null) {
+            date = new Date();
+            _user.setModified_time(date);
             _user.setUser_status(false);
             userRepository.save(_user);
             return new ResponseEntity<>("Delete user successfully!", HttpStatus.OK);
@@ -164,6 +166,8 @@ public class UserController {
     public ResponseEntity<?> recoverUser(@PathVariable("id") int id) {
         User _user = userRepository.getUserByUser_id(id);
         if (_user != null) {
+            date = new Date();
+            _user.setModified_time(date);
             _user.setUser_status(true);
             userRepository.save(_user);
             return new ResponseEntity<>("Recover user successfully!", HttpStatus.OK);
