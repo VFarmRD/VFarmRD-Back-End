7e33f7600bb14750cadc408573418226779f5909
minhnln0712 <qazzaq123>
2022-06-02 10:01:23 +0700

    add filter for get all user and product

--- src/main/java/com/example/vfarmrdbackend/controllers/UserController.java
+++ src/main/java/com/example/vfarmrdbackend/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.example.vfarmrdbackend.controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.vfarmrdbackend.models.Role;
 import com.example.vfarmrdbackend.models.User;
@@ -12,6 +15,9 @@ import com.example.vfarmrdbackend.repositories.UserRepository;
 import com.example.vfarmrdbackend.repositories.UserRoleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,6 +27,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -42,15 +49,33 @@ public class UserController {
 
     @GetMapping("/users")
     @PreAuthorize("hasAuthority('admin')")
-    public ResponseEntity<?> getAllUsers() {
+    public ResponseEntity<?> getAllUsers(@RequestParam(defaultValue = "", required = false) String user_name,
+            @RequestParam(defaultValue = "", required = false) String email,
+            @RequestParam(defaultValue = "", required = false) String fullname,
+            @RequestParam(defaultValue = "", required = false) String phone,
+            @RequestParam(defaultValue = "", required = false) String role_name,
+            @RequestParam(defaultValue = "true", required = false) boolean user_status,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "1000") int size) {
         try {
-            List<User> _listUsers = userRepository.getAllUsers();
-            if (_listUsers.isEmpty()) {
-                return new ResponseEntity<>(
-                        "Can't found any user!",
-                        HttpStatus.NO_CONTENT);
+            List<User> _listUsers = new ArrayList<User>();
+            Pageable paging = PageRequest.of(page, size);
+            Page<User> pageUsers;
+            if (user_name != null || email != null || fullname != null ||
+                    phone != null || role_name != null) {
+                pageUsers = userRepository.findUserByFields("%" + user_name + "%",
+                        "%" + email + "%", "%" + fullname + "%", "%" + phone + "%",
+                        "%" + role_name + "%", user_status, paging);
+            } else {
+                pageUsers = userRepository.findAllUsers(paging);
             }
-            return new ResponseEntity<>(_listUsers, HttpStatus.OK);
+            _listUsers = pageUsers.getContent();
+            Map<String, Object> response = new HashMap<>();
+            response.put("users", _listUsers);
+            response.put("currentPage", pageUsers.getNumber());
+            response.put("totalItems", pageUsers.getTotalElements());
+            response.put("totalPages", pageUsers.getTotalPages());
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(
                     "The server is down!",
@@ -87,7 +112,7 @@ public class UserController {
         for (int i = 0; i < listUserRole.size(); i++) {
             if (listUserRole.get(i).getRole_id() != roleRepository.getRoleByRole_name(role_name)
                     .getRole_id()) {
-                        userRoleRepository.delete(listUserRole.get(i));
+                userRoleRepository.delete(listUserRole.get(i));
             }
         }
     }
