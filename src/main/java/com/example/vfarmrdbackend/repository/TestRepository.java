3a0f3189fb16119f6edda56085cf0d477d6d4142
minhnln0712 <minhnln0712@gmail.com>
2022-06-18 00:16:03 +0700

    change get all formula return

--- src/main/java/com/example/vfarmrdbackend/repository/TestRepository.java
+++ src/main/java/com/example/vfarmrdbackend/repository/TestRepository.java
@@ -15,4 +15,10 @@ public interface TestRepository extends JpaRepository<Test, Integer> {
 
     @Query(value = "select * from tests t where t.test_id = :test_id", nativeQuery = true)
     Test getTestByTest_id(@Param("test_id") int test_id);
+
+    @Query(value = "select * from tests t where t.formula_id = :formula_id and t.test_result = true", nativeQuery = true)
+    List<Test> getAllPassTestWithFormula_id(@Param("formula_id") int formula_id);
+
+    @Query(value = "select * from tests t where t.formula_id = :formula_id and t.test_result = false", nativeQuery = true)
+    List<Test> getAllNotPassTestWithFormula_id(@Param("formula_id") int formula_id);
 }
