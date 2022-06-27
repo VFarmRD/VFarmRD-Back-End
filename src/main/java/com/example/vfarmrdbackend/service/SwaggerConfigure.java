f869cc3a38e1e7e9910e63ae05aa44cc1f1329bb
minhnln0712 <minhnln0712@gmail.com>
2022-06-28 01:45:40 +0700

    add test standart

---
+++ src/main/java/com/example/vfarmrdbackend/service/SwaggerConfigure.java
@@ -0,0 +1,20 @@
+package com.example.vfarmrdbackend.service;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "VFarm RD API", version = "v1"))
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class SwaggerConfigure {
+    
+}
