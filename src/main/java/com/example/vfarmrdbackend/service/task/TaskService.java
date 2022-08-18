ee75eddf9a4585894ca6cc110691661edb2c231c
minhnln0712 <minhnln0712@gmail.com>
2022-08-18 22:42:04 +0700

    .

--- src/main/java/com/example/vfarmrdbackend/service/task/TaskService.java
+++ src/main/java/com/example/vfarmrdbackend/service/task/TaskService.java
@@ -92,13 +92,14 @@ public class TaskService {
         for (int i = 0; i < listTasks.size(); i++) {
             Task task = listTasks.get(i);
             TaskGetResponse newTaskInfo = new TaskGetResponse();
+            User user = userRepository.getUserByUser_id(task.getUser_id());
             ProjectGetResponse project = projectService.getProjectByProject_id(task.getProject_id(), jwt);
             newTaskInfo.setTask_id(task.getTask_id());
             newTaskInfo.setTask_name(task.getTask_name());
             newTaskInfo.setUser_id(task.getUser_id());
             newTaskInfo.setManager_user_id(project.getCreated_user_id());
-            newTaskInfo.setUser_name(requestUser.getUser_name());
-            newTaskInfo.setUser_role(requestUser.getRole_name());
+            newTaskInfo.setUser_name(user.getUser_name());
+            newTaskInfo.setUser_role(user.getRole_name());
             newTaskInfo.setProject_id(task.getProject_id());
             newTaskInfo.setProject_name(project.getProject_name());
             newTaskInfo.setCreated_date(task.getCreated_date());
@@ -191,24 +192,24 @@ public class TaskService {
         return listTasksResponse;
     }
 
-    public TaskGetResponse getTaskByTask_id(int task_id,String jwt) {
+    public TaskGetResponse getTaskByTask_id(int task_id, String jwt) {
         Task task = taskRepository.getTaskByTask_id(task_id);
-            TaskGetResponse newTaskInfo = new TaskGetResponse();
-            User user = userRepository.getUserByUser_id(task.getUser_id());
-            ProjectGetResponse project = projectService.getProjectByProject_id(task.getProject_id(), jwt);
-            newTaskInfo.setTask_id(task.getTask_id());
-            newTaskInfo.setTask_name(task.getTask_name());
-            newTaskInfo.setUser_id(task.getUser_id());
-            newTaskInfo.setManager_user_id(project.getCreated_user_id());
-            newTaskInfo.setUser_name(user.getUser_name());
-            newTaskInfo.setUser_role(user.getRole_name());
-            newTaskInfo.setProject_id(task.getProject_id());
-            newTaskInfo.setProject_name(project.getProject_name());
-            newTaskInfo.setCreated_date(task.getCreated_date());
-            newTaskInfo.setStart_date(task.getStart_date());
-            newTaskInfo.setEstimated_date(task.getEstimated_date());
-            newTaskInfo.setTask_status(task.getTask_status());
-            newTaskInfo.setDescription(task.getDescription());
+        TaskGetResponse newTaskInfo = new TaskGetResponse();
+        User user = userRepository.getUserByUser_id(task.getUser_id());
+        ProjectGetResponse project = projectService.getProjectByProject_id(task.getProject_id(), jwt);
+        newTaskInfo.setTask_id(task.getTask_id());
+        newTaskInfo.setTask_name(task.getTask_name());
+        newTaskInfo.setUser_id(task.getUser_id());
+        newTaskInfo.setManager_user_id(project.getCreated_user_id());
+        newTaskInfo.setUser_name(user.getUser_name());
+        newTaskInfo.setUser_role(user.getRole_name());
+        newTaskInfo.setProject_id(task.getProject_id());
+        newTaskInfo.setProject_name(project.getProject_name());
+        newTaskInfo.setCreated_date(task.getCreated_date());
+        newTaskInfo.setStart_date(task.getStart_date());
+        newTaskInfo.setEstimated_date(task.getEstimated_date());
+        newTaskInfo.setTask_status(task.getTask_status());
+        newTaskInfo.setDescription(task.getDescription());
         return newTaskInfo;
     }
 
