d8d4b856841508e9606856b43a0d161049d22700
unknown <winnhat2000@gmail.com>
2022-10-26 02:00:58 +0700

    delete swagger

--- src/main/java/com/example/vfarmrdbackend/config/WebSecurityConfig.java
+++ src/main/java/com/example/vfarmrdbackend/config/WebSecurityConfig.java
@@ -60,9 +60,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.cors().and().csrf().disable().authorizeRequests()
-        .antMatchers("/swagger",
-            "/swagger-ui/**",
-            "/swagger-ui.html",
+        .antMatchers(
+//                "/swagger",
+//            "/swagger-ui/**",
+//            "/swagger-ui.html",
             "/api/auth/**",
             "/api/files/download/**")
         .permitAll()
